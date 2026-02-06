76c7c297ba14eff9aedc300510014bdf41ceb428
Rodrigo Bellizzieri <rodrigo.bellizzieri@hotmail.com>
2026-02-06 19:33:16 -0300

    Ajustando os outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "web_public_ip" {
   description = "IP público da instância web"
-  value       = module.web.public_ip
+  value       = module.web-server.public_ip
 }
 
 output "web_vpc_id" {
   description = "ID da VPC criada"
-  value       = module.web.vpc_id
+  value       = module.web-server.vpc_id
 }
